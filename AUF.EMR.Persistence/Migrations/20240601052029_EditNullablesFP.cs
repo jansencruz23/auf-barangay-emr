afee43e4b2fe817b9cca7555023b03039e09ff35
Jansen Cruz <103548207+jansencruz23@users.noreply.github.com>
2024-06-01 16:12:11 +0800

    Reformat create and edit fp records

---
+++ AUF.EMR.Persistence/Migrations/20240601052029_EditNullablesFP.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AUF.EMR.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class EditNullablesFP : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsPlanningChildren",
+                table: "FamilyPlanningRecords",
+                type: "tinyint(1)",
+                nullable: false,
+                defaultValue: false,
+                oldClrType: typeof(bool),
+                oldType: "tinyint(1)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsReasonSideEffects",
+                table: "ClientTypes",
+                type: "tinyint(1)",
+                nullable: false,
+                defaultValue: false,
+                oldClrType: typeof(bool),
+                oldType: "tinyint(1)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsReasonMedical",
+                table: "ClientTypes",
+                type: "tinyint(1)",
+                nullable: false,
+                defaultValue: false,
+                oldClrType: typeof(bool),
+                oldType: "tinyint(1)",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsPlanningChildren",
+                table: "FamilyPlanningRecords",
+                type: "tinyint(1)",
+                nullable: true,
+                oldClrType: typeof(bool),
+                oldType: "tinyint(1)");
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsReasonSideEffects",
+                table: "ClientTypes",
+                type: "tinyint(1)",
+                nullable: true,
+                oldClrType: typeof(bool),
+                oldType: "tinyint(1)");
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsReasonMedical",
+                table: "ClientTypes",
+                type: "tinyint(1)",
+                nullable: true,
+                oldClrType: typeof(bool),
+                oldType: "tinyint(1)");
+        }
+    }
+}
