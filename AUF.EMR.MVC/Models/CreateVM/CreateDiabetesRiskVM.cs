3f8d5495d329b3235323376690f9fd54066f7fbd
Jansen Cruz <jansencruz12345@gmail.com>
2024-12-26 17:33:58 +0800

    ✨: Display risk score

--- AUF.EMR.MVC/Models/CreateVM/CreateDiabetesRiskVM.cs
+++ AUF.EMR.MVC/Models/CreateVM/CreateDiabetesRiskVM.cs
@@ -1,5 +1,6 @@
 ﻿using AUF.EMR.Domain.Models;
 using AUF.EMR.MVC.Models.Common;
+using System.ComponentModel.DataAnnotations;
 
 namespace AUF.EMR.MVC.Models.CreateVM
 {
@@ -7,8 +8,14 @@ namespace AUF.EMR.MVC.Models.CreateVM
     {
         public List<HouseholdMember>? MemberList { get; set; } = new();
         public DiabetesRisk DiabetesRisk { get; set; } = new();
+
+        [Display(Name = "Height")]
         public double? HeightInCm { get; set; }
+
+        [Display(Name = "Weight")]
         public double? WeightInKg { get; set; }
+
+        [Display(Name = "BMI")]
         public double? Bmi { get; set; }
     }
 }
